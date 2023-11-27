159841a19801df7e741ee78d835640a4181d4444
krtek99 <mozgaviktor@gmail.com>
2023-11-27 13:17:47 +0100

    created  IRepository.cs, Repository.cs

---
+++ FCW0VU_HFT_2023241/FCW0VU_HFT_2023241.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using FCW0VU_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCW0VU_HFT_2023241.Repository
+{
+    internal interface IRepository<T> where T : Entity
+    {
+        void Create(T item);
+        T Read(int Id);
+        IQueryable<T> ReadAll();
+        void Update(T item);
+        void Delete(int Id);
+    }
+}
---
+++ FCW0VU_HFT_2023241/FCW0VU_HFT_2023241.Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using FCW0VU_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCW0VU_HFT_2023241.Repository
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected EducationDbContext ctx;
+
+        public Repository(EducationDbContext ctx)
+        {
+            this.ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var x = Read(id);
+            ctx.Set<T>().Remove(x);
+            ctx.SaveChanges();
+        }
+
+        public T Read(int Id)
+        {
+            return ctx.Set<T>().FirstOrDefault(x => x.Id == Id);
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Update(T item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
