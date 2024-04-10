a128b05999615c95e53fd2aebf8a3a9e019e886c
krtek99 <mozgaviktor@gmail.com>
2024-04-10 16:23:16 +0200

    wpf 1

---
+++ FCW0VU_HFT_2023241/FCW0VU_HFT_2023241.WPFclient/EmployeeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using FCW0VU_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FCW0VU_HFT_2023241.WPFclient
+{
+    public class EmployeeViewModel
+    {
+        public RestCollection<Employee> Employees { get; set; }
+
+        public EmployeeViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Employees = new RestCollection<Employee>("http://localhost:13109/", "Employee");
+            }
+        }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+    }
+}
